f40e0143ab35fe76bd27b5e1ed4680f337d071bd
night <yseddiq@gmail.com>
2023-05-01 19:00:41 -0400

    forgot to save before last push

---
+++ src/Components/Navigation/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+//this page will have all the navigatrion elements that should render on all pages.
+
+function Navigation({handlePageChange}){
+    
+}
