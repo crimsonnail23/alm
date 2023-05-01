c1d65772dc0f61bf72513123b8137e792925198f
night <yseddiq@gmail.com>
2023-05-01 18:49:23 -0400

    got really basic About section to render

---
+++ src/Components/About/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function About(){
+    return(
+        <section>
+            <h1>About ALM Watches</h1>
+            <p>
+                ALM watches sells and repairs watches.
+            </p>
+        </section>
+    )
+}
+
+export default About
